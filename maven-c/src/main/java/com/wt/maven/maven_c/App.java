1e0269dc06387be5ea5bf4becf0b5edf4cb702f1
hohoTT <609029365@qq.com>
2016-06-02 15:55:31 +0800

    创建三个maven项目工程，用于练习maven中的传递依赖的关系

---
+++ maven-c/src/main/java/com/wt/maven/maven_c/App.java
@@ -0,0 +1,13 @@
+package com.wt.maven.maven_c;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
